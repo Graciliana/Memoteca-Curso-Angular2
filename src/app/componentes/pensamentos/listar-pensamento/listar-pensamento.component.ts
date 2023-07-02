e08364c104a6abb94f2b44f67b00abc85fdeeb1e
Graciliana Kascher <eagraciliana@gmail.com>
2023-07-02 19:27:48 -0300

    :sparkles: feat: Realizando a paginacao dos pensamentos

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -5,18 +5,25 @@ import { Pensamento } from '../pensamento';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
-
   listaPensamentos: Pensamento[] = [];
-paginaAtual = 1
-  constructor(private service: PensamentoService) { }
+  paginaAtual = 1;
+  haMaisPensamentos: boolean = true;
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
     this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
-      this.listaPensamentos = listaPensamentos
-    })
+      this.listaPensamentos = listaPensamentos;
+    });
+  }
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
+    });
   }
 }
-
